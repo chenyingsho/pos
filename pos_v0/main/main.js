2a4fa35970e8cc5cf7bde78814dff4daaa2a1af1
chenyingsho <601668970@qq.com>
2016-05-04 20:58:19 +0800

    modify main

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,4 +1,20 @@
 //TODO: Please write code in this file.
 function printReceipt(inputs) {
+  var result='';
 
+  result="***<没钱赚商店>收据***\n";
+  for(var i=0;i<inputs.length;i++){
+    var total=1;
+    total=inputs[i].price*inputs[i].count;
+    result+="名称："+inputs[i].name+"，数量："+inputs[i].count+inputs[i].unit+"，单价："+inputs[i].price.toFixed(2)+"(元)，小计："+total.toFixed(2)+"(元)\n";
+  }
+  result+="----------------------\n";
+  var totalprice=0;
+  for(var j=0;j<inputs.length;j++) {
+
+    totalprice+=inputs[j].price*inputs[j].count;
+  }
+  result+="总计：" +totalprice.toFixed(2)+"(元)\n";
+  result+="**********************";
+  console.log(result);
 }
